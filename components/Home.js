518cd5bb5ed437d2a288af4a2e381dd6a364c8ed
Nitya Pandeti <nitya375@gmail.com>
2020-07-09 00:34:43 -0500

    Adding incremental CSS

--- components/Home.js
+++ components/Home.js
@@ -7,12 +7,42 @@
  */
 
 import React, { Component } from 'react';
-import { Button, Text, View, Image, TouchableOpacity, Dimensions } from 'react-native';
+import { Button, Text, View, Image, TouchableOpacity, Dimensions, StyleSheet, ImageBackground  } from 'react-native';
 
 import styles from "./Styles"
 import Splash from './Splash';
 
 const {width,height}= Dimensions.get("window");
+
+const styling = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+  terraPlanets: {
+    width: width/4,
+    height: height/4,
+  },
+  terraPlanetsLabel: {
+      width: width/5,
+      height: height/6,
+  },
+  gasGiants: {
+    width: width/4,
+    height: height/4,
+  },
+  gasGiantsLabel: {
+    width: width/5,
+    height: height/13,
+  },
+  asteroidBelt: {
+    width: width/4,
+    height: height/4,
+  },
+  asteroidBeltLabel: {
+    width: width/5,
+    height: height/6,
+  },
+});
 
 export default class Home extends Component {
   constructor(props){
@@ -42,30 +72,58 @@ setTimePassed() {
     else{ 
       return (
         <View>
-        <Image source={require('./images/space.jpg')} style= {styles.backgroundImage}/>
-        <TouchableOpacity testID="HomePageButtonToARWorld1" style={{position:'absolute'}} activeOpacity={0.5} onPress={() => this.props.navigation.navigate('Terrestrial Planets')}>
-            <Image
-            testID="HomePageImageWorld1"
-            source={require('./images/gasgiant.png')}
-            style={styles.ImageIconStyle}
-            />
-        </TouchableOpacity>
+        <ImageBackground source={require('./images/space.jpg')}  style={{width: width, height: height}}>
+            <TouchableOpacity
+                style={styling.terraPlanets}
+                testID="HomePageButtonToARWorld1"
+                //style={{position:'absolute'}}
+                activeOpacity={0.5}
+                onPress={() => this.props.navigation.navigate('Terrestrial Planets')}>
+                <Image
+                    style={styling.terraPlanets}
+                    testID="HomePageImageWorld1"
+                    source={require('./images/gasgiant.png')}
+                    //style={styles.ImageIconStyle}
+                />
+                <Image
+                    style={styling.terraPlanetsLabel}
+                    source={require('./images/terrestrialLabel.png')}/>
+            </TouchableOpacity>
 
-        <TouchableOpacity style={{position:'absolute', width: 100, height: 100, top:0}} testID="HomePageButtonToARWorld2" activeOpacity={0.5} onPress={() => this.props.navigation.navigate('Gas Giants')}>
-            <Image
-            testID="HomePageImageWorld2"
-            source={require('./images/gasgiant.png')}
-            style={styles.ImageIconStyle}
-            />
-        </TouchableOpacity>
+            <TouchableOpacity
+                //style={{position:'absolute', width: 100, height: 100, top:0}}
+                style={styling.gasGiants}
+                testID="HomePageButtonToARWorld2"
+                activeOpacity={0.5}
+                onPress={() => this.props.navigation.navigate('Gas Giants')}>
+                <Image
+                    style={styling.gasGiants}
+                    testID="HomePageImageWorld2"
+                    source={require('./images/gasgiant.png')}
+                    //style={styles.ImageIconStyle}
+                />
+                <Image
+                    style={styling.gasGiantsLabel}
+                    source={require('./images/gianttext.png')}/>
+            </TouchableOpacity>
 
-        <TouchableOpacity style={{position:'absolute'}} testID="HomePageButtonToARWorld3" activeOpacity={0.5} onPress={() => this.props.navigation.navigate('Asteroid Belt')}>
-            <Image
-            testID="HomePageImageWorld3"
-            source={require('./images/gasgiant.png')}
-            style={styles.ImageIconStyle}
-            />
-        </TouchableOpacity>
+            <TouchableOpacity
+                //style={{position:'absolute'}}
+                style={styling.asteroidBelt}
+                testID="HomePageButtonToARWorld3"
+                activeOpacity={0.5}
+                onPress={() => this.props.navigation.navigate('Asteroid Belt')}>
+                <Image
+                    style={styling.asteroidBelt}
+                    testID="HomePageImageWorld3"
+                    source={require('./images/gasgiant.png')}
+                    //style={styles.ImageIconStyle}
+                />
+                <Image
+                    style={styling.asteroidBeltLabel}
+                    source={require('./images/asteroidLabel.png')}/>
+            </TouchableOpacity>
+        </ImageBackground>
 
         </View>
     );}
